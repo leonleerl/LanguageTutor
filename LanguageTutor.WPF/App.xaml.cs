87362702523e5589508adb053df486b42add0682
locodurian <leon.runlongli@gmail.com>
2024-04-17 12:56:35 +0800

    initial commit

---
+++ LanguageTutor.WPF/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using LanguageTutor.WPF.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace LanguageTutor.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<KoreanTutor.WPF.KoreanTutorModule>();
+            moduleCatalog.AddModule<JapaneseTutor.WPF.JapaneseTutorModule>();
+        }
+    }
+}
---
+++ LanguageTutor.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LanguageTutor.WPF.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+
+        public MainViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            OpenCommand = new DelegateCommand<string>(Open);
+        }
+        
+        private void Open(string url)
+        {
+            switch (url)
+            {
+                case "Japanese":
+                    _regionManager.RequestNavigate("ContentRegion", nameof(JapaneseTutor.WPF.Views.MainView));
+                    break;
+                case "Korean":
+                    _regionManager.RequestNavigate("ContentRegion", nameof(KoreanTutor.WPF.Views.MainView));
+                    break;
+                case "Spanish":
+                    MessageBox.Show("Spanish");
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+
+        public DelegateCommand<string> OpenCommand { get; private set; }
+    }
+}
